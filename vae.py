6614e7c03aae0887cc1dc9c6c4eaeb9f6876b50d
Agustinus Kristiadi <kristiadi7@gmail.com>
2016-12-07 19:25:25 +0700

    Update loss to cross entropy

--- vae.py
+++ vae.py
@@ -62,7 +62,7 @@ outputs = decoder_out(h_p)
 def vae_loss(y_true, y_pred):
     """ Calculate loss = reconstruction loss + KL loss for each data in minibatch """
     # E[log P(X|z)]
-    recon = K.sum(K.square(y_true - y_pred), axis=1)
+    recon = K.sum(K.binary_crossentropy(y_pred, y_true), axis=1)
     # D_KL(Q(z|X) || P(z)); calculate in closed form as both dist. are Gaussian
     kl = 0.5 * K.sum(K.exp(log_sigma) + K.square(mu) - 1. - log_sigma, axis=1)
 
