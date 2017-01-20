c70281aadc154876b46b9b77613bf0ebc03ac71a
Agustinus Kristiadi <kristiadi7@gmail.com>
2017-01-20 18:09:39 +0700

    Add VAE & CVAE implementation in pytorch

---
+++ VAE/cvae_pytorch.py
@@ -0,0 +1,133 @@
+import torch
+import torch.nn.functional as nn
+import torch.autograd as autograd
+import torch.optim as optim
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.gridspec as gridspec
+import os
+from torch.autograd import Variable
+from tensorflow.examples.tutorials.mnist import input_data
+
+
+mnist = input_data.read_data_sets('../MNIST_data', one_hot=True)
+mb_size = 64
+Z_dim = 100
+X_dim = mnist.train.images.shape[1]
+y_dim = mnist.train.labels.shape[1]
+h_dim = 128
+cnt = 0
+lr = 1e-3
+
+
+def xavier_init(size):
+    in_dim = size[0]
+    xavier_stddev = 1. / np.sqrt(in_dim / 2.)
+    return Variable(torch.randn(*size) * xavier_stddev, requires_grad=True)
+
+
+def create_bias(size):
+    return Variable(torch.zeros(mb_size, size), requires_grad=True)
+
+
+# =============================== Q(z|X) ======================================
+
+Wxh = xavier_init(size=[X_dim + y_dim, h_dim])
+bxh = create_bias(h_dim)
+
+Whz_mu = xavier_init(size=[h_dim, Z_dim])
+bhz_mu = create_bias(Z_dim)
+
+Whz_var = xavier_init(size=[h_dim, Z_dim])
+bhz_var = create_bias(Z_dim)
+
+
+def Q(X, c):
+    inputs = torch.cat([X, c], 1)
+    h = nn.relu(inputs @ Wxh + bxh)
+    z_mu = h @ Whz_mu + bhz_mu
+    z_var = h @ Whz_var + bhz_var
+    return z_mu, z_var
+
+
+def sample_z(mu, log_var):
+    eps = Variable(torch.randn(mb_size, Z_dim))
+    return mu + torch.exp(log_var / 2) * eps
+
+
+# =============================== P(X|z) ======================================
+
+Wzh = xavier_init(size=[Z_dim + y_dim, h_dim])
+bzh = create_bias(h_dim)
+
+Whx = xavier_init(size=[h_dim, X_dim])
+bhx = create_bias(X_dim)
+
+
+def P(z, c):
+    inputs = torch.cat([z, c], 1)
+    h = nn.relu(inputs @ Wzh + bzh)
+    X = nn.sigmoid(h @ Whx + bhx)
+    return X
+
+
+# =============================== TRAINING ====================================
+
+params = [Wxh, bxh, Whz_mu, bhz_mu, Whz_var, bhz_var,
+          Wzh, bzh, Whx, bhx]
+
+solver = optim.Adam(params, lr=lr)
+
+for it in range(100000):
+    X, c = mnist.train.next_batch(mb_size)
+    X = Variable(torch.from_numpy(X))
+    c = Variable(torch.from_numpy(c.astype('float32')))
+
+    # Forward
+    z_mu, z_var = Q(X, c)
+    z = sample_z(z_mu, z_var)
+    X_sample = P(z, c)
+
+    # Loss
+    recon_loss = nn.binary_cross_entropy(X_sample, X, size_average=False)
+    kl_loss = 0.5 * torch.sum(torch.exp(z_var) + z_mu**2 - 1. - z_var)
+    loss = recon_loss + kl_loss
+
+    # Backward
+    loss.backward()
+
+    # Update
+    solver.step()
+
+    # Housekeeping
+    for p in params:
+        p.grad.data.zero_()
+
+    # Print and plot every now and then
+    if it % 1000 == 0:
+        print('Iter-{}; loss: {}'.format(it, loss))
+
+        c = np.zeros(shape=[mb_size, y_dim], dtype='float32')
+        c[:, np.random.randint(0, 10)] = 1.
+        c = Variable(torch.from_numpy(c))
+        z = Variable(torch.randn(mb_size, Z_dim))
+        samples = P(z, c).data.numpy()[:16]
+
+        fig = plt.figure(figsize=(4, 4))
+        gs = gridspec.GridSpec(4, 4)
+        gs.update(wspace=0.05, hspace=0.05)
+
+        for i, sample in enumerate(samples):
+            ax = plt.subplot(gs[i])
+            plt.axis('off')
+            ax.set_xticklabels([])
+            ax.set_yticklabels([])
+            ax.set_aspect('equal')
+            plt.imshow(sample.reshape(28, 28), cmap='Greys_r')
+
+        if not os.path.exists('out/'):
+            os.makedirs('out/')
+
+        plt.savefig('out/{}.png'.format(str(cnt).zfill(3)), bbox_inches='tight')
+        cnt += 1
+        plt.close(fig)
