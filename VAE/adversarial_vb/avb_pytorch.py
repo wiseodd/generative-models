Fix ELBO term scaling

VAE/adversarial_vb/avb_pytorch.py
@@ -83,7 +83,7 @@ for it in range(1000000):
     T_sample = T(torch.cat([X, z_sample], 1))
 
     disc = torch.mean(-T_sample)
-    loglike = -nn.binary_cross_entropy(X_sample, X)
+    loglike = -nn.binary_cross_entropy(X_sample, X, size_average=False) / mb_size
 
     elbo = -(disc + loglike)
 

VAE/adversarial_vb/avb_tensorflow.py
@@ -107,9 +107,11 @@ D_prior = tf.nn.sigmoid(D(X, z))
 X_samples, _ = P(z)
 
 disc = tf.reduce_mean(-D_sample)
-loglike = -tf.reduce_mean(
-    tf.nn.sigmoid_cross_entropy_with_logits(logits=X_logits, labels=X)
+nll = tf.reduce_sum(
+    tf.nn.sigmoid_cross_entropy_with_logits(logits=X_logits, labels=X),
+    axis=1
 )
+loglike = -tf.reduce_mean(nll)
 
 elbo = disc + loglike
 D_loss = tf.reduce_mean(log(D_q) + log(1. - D_prior))