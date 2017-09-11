86338b3f3198b023b4d2414b370f77f8b8e0791e
Mark <mark3ting.sci3nc3s@gmail.com>
2017-09-11 18:01:45 +0200

    add -u to python3 command for unbuffering

--- run.bash
+++ run.bash
@@ -2,9 +2,13 @@
 C=0
 theDate=$(date +%Y-%m-%d)
 if [ ! -d log ];then
-    mkdir $log
+    mkdir log
 fi
 for i in $(find GAN RBM VAE -name "*.py");do
-    logfile=$log/$(echo $i | awk -F '/' '{print $3}').$C.$theDate.log
-    (export CUDA_VISIBLE_DEVICE=$C;annotate-output +"%Y-%m-%d %H:%M:%S" time python3 $i |& tee $logfile)
+    script=$(echo $i | awk -F '/' '{print $3}')
+    logfile=$PWD/log/$script.$C.$theDate.log
+    dirmod=$(echo $i | awk -F '/' '{print $1}')/$(echo $i | awk -F '/' '{print $2}')
+    pushd $dirmod > /dev/null
+    (export CUDA_VISIBLE_DEVICE=$C;annotate-output +"%Y-%m-%d %H:%M:%S" time python3 -u $script |& tee $logfile)
+    popd > /dev/null
 done
